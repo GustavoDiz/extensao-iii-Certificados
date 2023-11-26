e414f5a5c359ad20437a914118ba107399c52826
Gustavo Diogenes de Souza <gustavodio1999@gmail.com>
2023-11-25 21:49:59 -0300

    att background image

--- client/scripts/meu_eventos.js
+++ client/scripts/meu_eventos.js
@@ -3,7 +3,7 @@ let content = {};
 async function loadEvents(){
     var ob = localStorage.getItem("user");
     console.log(JSON.parse(ob).id);
-    let eventos = document.getElementById('myevents');
+    let eventos = document.getElementById('events-content');
     await fetch(`http://localhost:5000/api/events/myevents/${JSON.parse(ob).id}`,{
         method: 'GET',
         headers: {
@@ -13,6 +13,17 @@ async function loadEvents(){
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
+        content = data.events;
     })
     .catch((error) => console.log("Erro:", error));
+    if(content != null){
+        content.map((event)=>{
+            eventos.innerHTML += `<div class="bg-card">
+                <h2>${event.title}</h2>
+                <a href="../evento/index.html?id=${event.id}" id="link"><button class="signevent_button">Gerenciar</button></a>
+        </div> `;
+        })
+    }else{
+        eventos.innerHTML = "<h1>Nenhum Evento Encontrado :(</h1>";
+    }
 }
